af6e63d5cd64217b59810593b18e174e15defd1d
Samuel Fabian <sfabian@ramsinternationalgc.com>
2026-02-01 21:09:40 -0500

    FRONTEND: force redeploy (login page update)

--- app/login/page.js
+++ app/login/page.js
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 
+export const dynamic = "force-dynamic";
 export default function LoginPage() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
